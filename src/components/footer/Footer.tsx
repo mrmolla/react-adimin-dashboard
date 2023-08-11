5e018709e6f798bfe90cf04e6e9dacd6fd440a78
mrmolla <115748318+mrmolla@users.noreply.github.com>
2023-08-12 02:22:24 +0330

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>page prefect</span>
+      <span>© page prefect Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
